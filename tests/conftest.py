12695bb2aab2797fe3637901413baee19a54cc39
jmcarp <jm.carp@gmail.com>
2014-10-08 11:37:26 -0400

    Ensure different HMAC secrets for all signers in testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,7 @@ def ensure_different_hmac_secrets(monkeypatch):
     """Ensure that HMAC secret keys for uploads and webhooks are different so
     that tests fail if we use the wrong keys.
     """
-    monkeypatch.setattr(
-        settings,
-        'WEBHOOK_HMAC_SECRET',
-        settings.UPLOAD_HMAC_SECRET + 'different'
-    )
+    monkeypatch.setattr(settings, 'URLS_HMAC_SECRET', 'urls-secret')
+    monkeypatch.setattr(settings, 'UPLOAD_HMAC_SECRET', 'upload-secret')
+    monkeypatch.setattr(settings, 'WEBHOOK_HMAC_SECRET', 'webhook-secret')
 
